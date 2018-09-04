f451319b6703175a39d8050b03775cbdd687f8ed
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-09-04 17:28:43 +0200

    Setup libGDX project

---
+++ desktop/src/org/cafebabe/olgs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.cafebabe.olgs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.cafebabe.olgs.OpenLogicGateSimulator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OpenLogicGateSimulator(), config);
+	}
+}
