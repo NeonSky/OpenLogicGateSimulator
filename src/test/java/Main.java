53af4429f499a41fc72d739f2ff40434e03116ae
pilino1234 <pilino1234@zoho.eu>
2018-09-11 13:02:23 +0200

    Add unit test infrastructure
    New unit tests should be added in an appropriate class in src/test/ and
    use JUnit5.
    
    Gradle can be used to run the unit tests with `./gradlew test`. See
    README.md for instructions on setting up a run configuration for this
    from within IntelliJ.
    
    Travis will run the unit tests as part of every build.

---
+++ src/test/java/Main.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MyTests {
+
+    @Test
+    void dummyTest() {
+        System.out.println("### This test method should be run ###");
+        assertEquals(1, 1);
+    }
+}
