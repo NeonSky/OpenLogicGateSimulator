72f5ae417a2791fce93cdf2f7adab9ea4dae7504
Jimmy M Andersson <hello@applyn.se>
2018-09-12 17:53:31 +0200

    Add styling and setter methods to ComponentListCell

--- src/main/java/org/cafebabe/controllers/ComponentListCell.java
+++ src/main/java/org/cafebabe/controllers/ComponentListCell.java
@@ -3,6 +3,7 @@ package org.cafebabe.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
@@ -11,19 +12,20 @@ import java.io.IOException;
 public class ComponentListCell extends AnchorPane {
 
     // Instance variables
-    @FXML AnchorPane componentCell;
-    @FXML ImageView componentImage;
-    @FXML Label componentNameLabel;
+    @FXML private AnchorPane componentCell;
+    @FXML private ImageView componentImage;
+    @FXML private Label componentNameLabel;
 
     // Constructors
     public ComponentListCell() throws RuntimeException {
-        this.loadFXML();
+        this.loadFXML("view/ComponentListCell.fxml");
         this.bindSizeProperties();
+
     }
 
     // Instance methods
-    private void loadFXML() {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/ComponentListCell.fxml"));
+    private void loadFXML(String fileName) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fileName));
         loader.setController(this);
         loader.setRoot(this);
 
@@ -34,10 +36,34 @@ public class ComponentListCell extends AnchorPane {
         }
     }
 
+    /**
+     * Binds the size attributes of the component cell and the component image to this' size.
+     */
     private void bindSizeProperties() {
         this.componentCell.prefHeightProperty().bind(this.heightProperty().subtract(20));
         this.componentCell.prefWidthProperty().bind(this.widthProperty().subtract(20));
         this.componentImage.fitHeightProperty().bind(this.componentCell.widthProperty().subtract(30));
         this.componentImage.fitWidthProperty().bind(this.componentCell.widthProperty().subtract(30));
+    }
+
+    /**
+     * Sets the text attribute of this component cells name label.
+     * @param name The name to display
+     */
+    private void setComponentNameLabel(String name) {
+        if (name.isEmpty() || name == null) { return; }
+
+        String upperCasedName = name.toUpperCase();
+        this.componentNameLabel.setText(upperCasedName);
+    }
+
+    /**
+     * Sets the image to be displayed by this cell.
+     * @param image The image to be displayed
+     */
+    private void setComponentImage(Image image) {
+        if (image == null) { return; }
+
+        this.componentImage.setImage(image);
     }
 }
