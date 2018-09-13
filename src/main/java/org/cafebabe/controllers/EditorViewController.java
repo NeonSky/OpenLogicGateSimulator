820216725649c89185181ce8c1a2cc04b30b3bdd
Elias Sundqvist <elias@sundqvist.com>
2018-09-13 14:05:49 +0200

    Add dummy components to the sidebar
    ref #7

--- src/main/java/org/cafebabe/controllers/EditorViewController.java
+++ src/main/java/org/cafebabe/controllers/EditorViewController.java
@@ -5,11 +5,19 @@ import javafx.scene.layout.Pane;
 import javafx.fxml.FXML;
 import org.cafebabe.model.components.MockComponent;
 import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class EditorViewController implements Initializable {
 
     @FXML private Pane ROOT_PANE;
+    @FXML private FlowPane componentFlowPane;
 
     private ComponentController mockComponent;
 
@@ -18,5 +26,15 @@ public class EditorViewController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         mockComponent = new ComponentController(new MockComponent());
         ROOT_PANE.getChildren().add(mockComponent);
+        addDummyComponents();
+    }
+
+    private void addDummyComponents() {
+        List<ComponentListCell> listCells = new ArrayList<>();
+        for(int i = 0; i<100; i++) {
+            listCells.add(new ComponentListCell());
+        }
+
+        componentFlowPane.getChildren().addAll(listCells);
     }
 }
