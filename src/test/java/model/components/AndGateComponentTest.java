202895e844f94ec07b8bc343dcd30a1e3ffb55b5
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-09-13 17:02:29 +0200

    Add AND-Gate component

---
+++ src/test/java/model/components/AndGateComponentTest.java
@@ -0,0 +1,44 @@
+package model.components;
+
+import org.cafebabe.model.components.PowerSourceComponent;
+import org.cafebabe.model.components.AndGateComponent;
+import org.cafebabe.model.components.connections.Wire;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AndGateComponentTest {
+
+    @Test
+    void metadataTest() {
+        AndGateComponent comp = new AndGateComponent();
+        assertEquals("AND_ANSI", comp.getAnsiName());
+        assertEquals("AND-Gate", comp.getDisplayName());
+        assertEquals("Emits an active signal if both inputs are active", comp.getDescription());
+    }
+
+    @Test
+    void intendedUseTest() {
+        PowerSourceComponent power1 = new PowerSourceComponent();
+        PowerSourceComponent power2 = new PowerSourceComponent();
+        Wire in1 = new Wire();
+        Wire in2 = new Wire();
+        Wire out = new Wire();
+        AndGateComponent comp = new AndGateComponent();
+
+        // Connect wires to power sources
+        power1.connectToPort(in1, "north");
+        power2.connectToPort(in2, "north");
+
+        // initial state should off
+        comp.connectToPort(out, "output");
+        assertEquals(false, out.isActive());
+
+        // AND-Gate output should only be 1 when both inputs are on
+        comp.connectToPort(in1, "input1");
+        assertEquals(false, out.isActive());
+        comp.connectToPort(in2, "input2");
+        assertEquals(true, out.isActive());
+
+    }
+}
