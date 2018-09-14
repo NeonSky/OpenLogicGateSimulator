8c9ce332d95be955564d7784716bee1a2d8dd077
pilino1234 <pilino1234@zoho.eu>
2018-09-14 13:53:04 +0200

    Prepare EditorView for adding Workspace/Circuit drawing

--- src/main/java/org/cafebabe/controllers/EditorViewController.java
+++ src/main/java/org/cafebabe/controllers/EditorViewController.java
@@ -14,9 +14,15 @@ public class EditorViewController implements Initializable {
     @FXML private Pane ROOT_PANE;
     @FXML private FlowPane componentFlowPane;
     @FXML private AnchorPane sidebarAnchorPane;
+    @FXML private AnchorPane workspacesPane;
+
+    private ComponentController mockComponent;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         sidebarAnchorPane.getChildren().add(new ComponentListController());
+        mockComponent = new ComponentController(new MockComponent(), 0, 0);
+        workspacesPane.getChildren().add(mockComponent);
     }
 }
