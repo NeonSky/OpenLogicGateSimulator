5483983cbdd09eff76d4db8038cf2a415e9496f7
Jimmy M Andersson <hello@applyn.se>
2018-09-14 16:08:13 +0200

    Add scaling to ComponentListCell SVG paths

--- src/main/java/org/cafebabe/controllers/ComponentListCell.java
+++ src/main/java/org/cafebabe/controllers/ComponentListCell.java
@@ -1,24 +1,19 @@
 package org.cafebabe.controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.SVGPath;
 import org.cafebabe.controllers.util.FxmlUtil;
 
-import java.io.IOException;
-
 public class ComponentListCell extends AnchorPane {
 
-    // Instance variables
     @FXML private AnchorPane componentCell;
     @FXML private SVGPath svg;
+    @FXML private GridPane grid;
     @FXML private Label componentNameLabel;
 
-    // Constructors
     public ComponentListCell(String name, String svgContent) throws RuntimeException {
         FxmlUtil.attachFXML(this, "/view/ComponentListCell.fxml");
         this.bindSizeProperties();
@@ -26,18 +21,11 @@ public class ComponentListCell extends AnchorPane {
         setComponentSvgContent(svgContent);
     }
 
-    /**
-     * Binds the size attributes of the component cell and the component image to this size.
-     */
     private void bindSizeProperties() {
         this.componentCell.prefHeightProperty().bind(this.heightProperty().subtract(20));
         this.componentCell.prefWidthProperty().bind(this.widthProperty().subtract(20));
     }
 
-    /**
-     * Sets the text attribute of this component cells name label.
-     * @param name The name to display
-     */
     private void setComponentNameLabel(String name) {
         if (name == null) {
             throw new RuntimeException("Can't set a label that is null or empty");
@@ -47,15 +35,21 @@ public class ComponentListCell extends AnchorPane {
         this.componentNameLabel.setText(upperCasedName);
     }
 
-    /**
-     * Sets the SVG content to be displayed by this cell.
-     * @param svgContent The SVG content to be displayed
-     */
     private void setComponentSvgContent(String svgContent) {
         if (svgContent == null) {
             throw new RuntimeException("Can't load svgContent that is null or empty");
         }
 
         this.svg.setContent(svgContent);
+
+        double width = this.svg.prefWidth(-1);
+        double height = this.svg.prefHeight(width);
+        double scaleX = this.grid.getPrefWidth() / width;
+        double scaleY = this.grid.getPrefHeight() / height;
+
+        double finalScaleFactor = Math.min(scaleX, scaleY);
+
+        this.svg.setScaleX(finalScaleFactor);
+        this.svg.setScaleY(finalScaleFactor);
     }
 }
