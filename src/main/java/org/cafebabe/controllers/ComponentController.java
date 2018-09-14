73408f374a0172080f66388ac386c119561245d0
pilino1234 <pilino1234@zoho.eu>
2018-09-14 13:34:04 +0200

    Add position awareness to ComponentController
    Create a new position class to allow ComponentControllers to store its
    position on the workspace grid.

--- src/main/java/org/cafebabe/controllers/ComponentController.java
+++ src/main/java/org/cafebabe/controllers/ComponentController.java
@@ -7,18 +7,26 @@ import javafx.scene.shape.SVGPath;
 import org.cafebabe.controllers.util.FxmlUtil;
 import org.cafebabe.controllers.util.SvgUtil;
 import org.cafebabe.model.components.Component;
+import org.cafebabe.model.workspace.Position;
 
 public class ComponentController extends AnchorPane {
 
     @FXML private SVGPath componentSvgPath;
 
     private Component component;
+    private Position position;
 
+    public ComponentController(Component component, int x, int y) {
+        this(component, new Position(x, y));
+    }
 
-    public ComponentController(Component component) {
+    public ComponentController(Component component, Position pos) {
         FxmlUtil.attachFXML(this, "/view/ComponentView.fxml");
 
         this.component = component;
         componentSvgPath.setContent(SvgUtil.getComponentSvgPath(component));
+
+        this.position = pos;
     }
+
 }
