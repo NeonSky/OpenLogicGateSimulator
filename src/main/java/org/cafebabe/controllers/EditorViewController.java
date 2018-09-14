c61fc6bd20f96d5cecab24b1652943bc28366a88
Elias Sundqvist <elias@sundqvist.com>
2018-09-14 16:26:04 +0200

    Add ability to load components dynamically through reflection

--- src/main/java/org/cafebabe/controllers/EditorViewController.java
+++ src/main/java/org/cafebabe/controllers/EditorViewController.java
@@ -6,17 +6,19 @@ import javafx.fxml.FXML;
 import java.net.URL;
 import javafx.scene.layout.FlowPane;
 import org.cafebabe.controllers.util.SvgUtil;
+import org.cafebabe.model.components.Component;
 import org.cafebabe.model.components.MockComponent;
+import org.reflections.Reflections;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class EditorViewController implements Initializable {
 
     @FXML private Pane ROOT_PANE;
     @FXML private FlowPane componentFlowPane;
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -24,10 +26,22 @@ public class EditorViewController implements Initializable {
     }
 
     private void addDummyComponents() {
-        MockComponent mock = new MockComponent();
         List<ComponentListCell> listCells = new ArrayList<>();
-        for(int i = 0; i < 100; i++) {
-            listCells.add(new ComponentListCell(mock.getDisplayName(), SvgUtil.getComponentSvgPath(mock)));
+
+        Reflections reflections = new Reflections("org.cafebabe.model.components");
+
+        Set<Class<? extends Component>> componentClasses =
+                reflections.getSubTypesOf(Component.class);
+
+        for(Class<? extends Component> componentClass : componentClasses) {
+            Component component = null;
+            try {
+                component = componentClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            if(component == null) continue;
+            listCells.add(new ComponentListCell(component.getDisplayName(), SvgUtil.getComponentSvgPath(component)));
         }
 
         componentFlowPane.getChildren().addAll(listCells);
