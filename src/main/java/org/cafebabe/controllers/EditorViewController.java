a1c75c11f2419ded8592139db28a15eae32b76fe
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-09-14 11:39:59 +0200

    Switch cell content from img to svg

--- src/main/java/org/cafebabe/controllers/EditorViewController.java
+++ src/main/java/org/cafebabe/controllers/EditorViewController.java
@@ -3,13 +3,11 @@ package org.cafebabe.controllers;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 import javafx.fxml.FXML;
-import org.cafebabe.model.components.MockComponent;
 import java.net.URL;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.layout.FlowPane;
+import org.cafebabe.controllers.util.SvgUtil;
+import org.cafebabe.model.components.MockComponent;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -19,20 +17,17 @@ public class EditorViewController implements Initializable {
     @FXML private Pane ROOT_PANE;
     @FXML private FlowPane componentFlowPane;
 
-    private ComponentController mockComponent;
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        mockComponent = new ComponentController(new MockComponent());
-        ROOT_PANE.getChildren().add(mockComponent);
         addDummyComponents();
     }
 
     private void addDummyComponents() {
+        MockComponent mock = new MockComponent();
         List<ComponentListCell> listCells = new ArrayList<>();
         for(int i = 0; i < 100; i++) {
-            listCells.add(new ComponentListCell());
+            listCells.add(new ComponentListCell(mock.getDisplayName(), SvgUtil.getComponentSvgPath(mock)));
         }
 
         componentFlowPane.getChildren().addAll(listCells);
