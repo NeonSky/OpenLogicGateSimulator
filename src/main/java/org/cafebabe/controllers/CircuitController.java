81d33a5ee3cbda8f785deac489b77c41464b2e7b
pilino1234 <pilino1234@zoho.eu>
2018-09-15 17:34:15 +0200

    Minor code cleanup

--- src/main/java/org/cafebabe/controllers/CircuitController.java
+++ src/main/java/org/cafebabe/controllers/CircuitController.java
@@ -1,7 +1,6 @@
 package org.cafebabe.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import org.cafebabe.controllers.util.CanvasGridPane;
@@ -12,7 +11,7 @@ import org.cafebabe.model.components.Component;
 import java.util.HashSet;
 import java.util.Set;
 
-public class CircuitController extends AnchorPane {
+class CircuitController extends AnchorPane {
 
     @FXML private Pane backgroundPane;
     private CanvasGridPane gridPane;
@@ -21,15 +20,13 @@ public class CircuitController extends AnchorPane {
     private final Circuit circuit;
     private Set<ComponentController> ccSet = new HashSet<>();
 
-    public CircuitController(Circuit circuit) {
+    CircuitController(Circuit circuit) {
         this.circuit = circuit;
-        //this.componentPane.getChildren().addAll(this.ccSet);
 
         FxmlUtil.attachFXML(this, "/view/CircuitView.fxml");
         FxmlUtil.scaleWithAnchorPaneParent(this);
 
         gridPane = new CanvasGridPane();
-        //gridPane.setStyle("-fx-background-color: darkblue;" );
         FxmlUtil.scaleWithAnchorPaneParent(gridPane);
         this.getChildren().add(gridPane);
 
@@ -38,19 +35,19 @@ public class CircuitController extends AnchorPane {
         this.componentPane.toFront();
     }
 
-    public void addComponent(Component component, int x, int y) {
+    void addComponent(Component component, int x, int y) {
         this.circuit.addComponent(component);
         this.ccSet.add(new ComponentController(component, x, y));
         update();
     }
 
-    public void removeComponent(ComponentController component) {
+    void removeComponent(ComponentController component) {
         this.circuit.removeComponent(component.getComponent());
         this.ccSet.remove(component);
         update();
     }
 
-    public void update() {
+    private void update() {
         this.componentPane.getChildren().clear();
         this.componentPane.getChildren().addAll(ccSet);
     }
