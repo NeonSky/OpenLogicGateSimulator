6af0b5b7cfcdcca4f62f0ca3b7bcc594e1eb43f4
pilino1234 <pilino1234@zoho.eu>
2018-09-15 18:33:42 +0200

    Circuit unit tests

---
+++ src/test/java/model/circuit/CircuitTest.java
@@ -0,0 +1,93 @@
+package model.circuit;
+
+import org.cafebabe.model.circuit.Circuit;
+import org.cafebabe.model.components.AndGateComponent;
+import org.cafebabe.model.components.Component;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircuitTest {
+
+    @Test
+    void Circuit() {
+        Circuit c = new Circuit();
+        assertTrue(c.getComponents().isEmpty());
+        assertTrue(c.getWires().isEmpty());
+    }
+
+    @Test
+    void addComponent() {
+        Circuit c = new Circuit();
+        c.addComponent(new AndGateComponent());
+        assertEquals(c.getComponents().size(), 1);
+    }
+
+    @Test
+    void addComponentMultiple() {
+        Circuit c = new Circuit();
+
+        for (int i=0; i < 10; i++) {
+            c.addComponent(new AndGateComponent());
+            assertEquals(c.getComponents().size(), i+1);
+        }
+    }
+
+    @Test
+    void addSameComponentSeveralTimes() {
+        Circuit c = new Circuit();
+        Component a = new AndGateComponent();
+
+        c.addComponent(a);
+        assertEquals(c.getComponents().size(), 1);  // adding once is fine
+        assertThrows(RuntimeException.class, () -> c.addComponent(a));  // adding again is error
+    }
+
+    @Test
+    void removeComponent() {
+        Circuit c = new Circuit();
+
+        AndGateComponent a = new AndGateComponent();
+
+        c.addComponent(a);
+        assertEquals(c.getComponents().size(), 1);
+        c.removeComponent(a);
+        assertTrue(c.getComponents().isEmpty());
+    }
+
+    @Test
+    void removeComponentsMultiple() {
+        Circuit c = new Circuit();
+
+        List<Component> componentList = new ArrayList<>();
+
+        for (int i=0; i<10; i++) {
+            AndGateComponent a = new AndGateComponent();
+            c.addComponent(a);
+            componentList.add(a);
+        }
+
+        for (Component component : componentList) {
+            c.removeComponent(component);
+        }
+
+        assertTrue(c.getComponents().isEmpty());
+    }
+
+    @Test
+    void removeNonexistentComponent() {
+        Circuit c = new Circuit();
+        assertThrows(RuntimeException.class, () -> c.removeComponent(new AndGateComponent()));
+    }
+
+    @Test
+    void addWire() { // TODO
+    }
+
+    @Test
+    void removeWire() { // TODO
+    }
+}
