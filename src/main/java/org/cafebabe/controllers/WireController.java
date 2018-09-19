9b2ec02e6c7eea06895e32914873d28a20041f69
Jimmy M Andersson <hello@applyn.se>
2018-09-19 10:20:24 +0200

    Make wires drawable
    Create a Bezier curve in WireController that can be used to draw wires onto the workspace.

--- src/main/java/org/cafebabe/controllers/WireController.java
+++ src/main/java/org/cafebabe/controllers/WireController.java
@@ -1,58 +1,93 @@
 package org.cafebabe.controllers;
 
+import javafx.scene.Node;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Effect;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.CubicCurve;
+import javafx.scene.shape.StrokeLineCap;
 import org.cafebabe.model.components.connections.Wire;
 import org.cafebabe.model.workspace.Position;
 
 public class WireController {
 
-    private static Color activeColor = Color.color(234, 38, 38, 1);
-    private static Color inactiveColor = Color.color(0, 0, 0, 1);
 
+    // Class properties
+    private static Color ACTIVE_COLOR = Color.color(234.0/255, 38.0/255, 38.0/255, 1);
+    private static Color INACTIVE_COLOR = Color.color(0, 0, 0, 1);
+    private static int WIRE_WIDTH = 4;
+
+
+    // Instance properties
     private Wire wire;
-    private Line wireLine;
+    private CubicCurve wireLine;
 
+
+    // Constructors
     WireController(Wire wire, int startX, int startY, int endX, int endY) {
         this(wire, new Position(startX, startY), new Position(endX, endY));
     }
 
     WireController(Wire wire, Position startPoint, Position endPoint) {
         this.wire = wire;
-        this.wireLine = new Line(startPoint.getX(), startPoint.getY(), endPoint.getX(), endPoint.getY());
-        this.wireLine.setFill(getWireColor());
+        this.wireLine = new CubicCurve();
+        moveStartPointTo(startPoint.getX(), startPoint.getY());
+        moveEndPointTo(endPoint.getX(), endPoint.getY());
+        setWireControlPoints();
+        setWireDrawingOptions();
         this.wire.onStateChangedEvent().addListener(this::updateState);
     }
 
-    void moveStartPointTo(int x, int y) {
-        this.wireLine.setStartX(x);
-        this.wireLine.setStartY(y);
+
+    // Private helper methods
+    private void moveStartPointTo(Number x, Number y) {
+        this.wireLine.setStartX(x.doubleValue());
+        this.wireLine.setStartY(y.doubleValue());
     }
 
-    void moveEndPointTo(int x, int y) {
-        this.wireLine.setEndX(x);
-        this.wireLine.setEndY(y);
+    private void moveEndPointTo(Number x, Number y) {
+        this.wireLine.setEndX(x.doubleValue());
+        this.wireLine.setEndY(y.doubleValue());
+        setWireControlPoints();
     }
 
-    void translateStartPoint(int deltaX, int deltaY) {
-        this.wireLine.setStartX(wireLine.getStartX() + deltaX);
-        this.wireLine.setStartY(wireLine.getStartY() + deltaY);
+    private Color getWireColor(Wire wire) {
+        return (wire.isActive()) ? ACTIVE_COLOR : INACTIVE_COLOR;
     }
 
-    void translateEndPoint(int deltaX, int deltaY) {
-        this.wireLine.setEndX(wireLine.getEndX() + deltaX);
-        this.wireLine.setEndY(wireLine.getEndY() + deltaY);
+    private void setWireDrawingOptions() {
+        this.wireLine.setStroke(this.getWireColor(this.wire));
+        this.wireLine.setFill(Color.TRANSPARENT);
+        this.wireLine.setStrokeWidth(WIRE_WIDTH);
+        this.wireLine.setStrokeLineCap(StrokeLineCap.ROUND);
+        Effect dropShadow = new DropShadow(BlurType.GAUSSIAN, Color.WHITE, 10, 0.2, 0, 0);
+        this.wireLine.setEffect(dropShadow);
     }
 
-    private Color getWireColor() {
-        return (this.wire.isActive()) ? activeColor : inactiveColor;
+    private void updateState(Wire wire) {
+        this.wireLine.setStroke(getWireColor(wire));
     }
 
-    public Line getWireLine() {
+    private void setWireControlPoints() {
+        double startX = this.wireLine.getStartX();
+        double startY = this.wireLine.getStartY();
+        double endX = this.wireLine.getEndX();
+        double endY = this.wireLine.getEndY();
+        this.wireLine.setControlX1((endX + startX) / 2);
+        this.wireLine.setControlY1(startY);
+        this.wireLine.setControlX2((endX + startX) / 2);
+        this.wireLine.setControlY2(endY);
+    }
+
+    // Public methods
+    public Node getWireLine() {
         return this.wireLine;
     }
 
-    void updateState(Wire wire) {
-        this.wireLine.setStroke(getWireColor());
+    public void moveLineTo(Position startPoint, Position endPoint) {
+        this.moveStartPointTo(startPoint.getX(), startPoint.getY());
+        this.moveEndPointTo(endPoint.getX(), endPoint.getY());
+        setWireControlPoints();
     }
 }
