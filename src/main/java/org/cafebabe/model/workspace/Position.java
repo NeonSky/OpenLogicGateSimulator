fe8d6dfece65c0c9a6f3d1741ea5321e3a154dc1
Jimmy M Andersson <hello@applyn.se>
2018-09-19 10:20:24 +0200

    Make wires drawable
    Create a Bezier curve in WireController that can be used to draw wires onto the workspace.

--- src/main/java/org/cafebabe/model/workspace/Position.java
+++ src/main/java/org/cafebabe/model/workspace/Position.java
@@ -8,9 +8,9 @@ public class Position {
         this(0, 0);
     }
 
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Position(Number x, Number y) {
+        this.x = x.intValue();
+        this.y = y.intValue();
     }
 
     public int getX() {
