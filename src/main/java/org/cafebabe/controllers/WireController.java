a12910d1eabfadd434b09b20c6b786e0320bdf0a
Jimmy M Andersson <hello@applyn.se>
2018-09-19 08:34:51 +0200

    Update and modify WireController class
    Replace startPoint and endPoint with a Line that can be used to update position and active state color. Sign up WireController to receive notifications about state changes in Wire class.

--- src/main/java/org/cafebabe/controllers/WireController.java
+++ src/main/java/org/cafebabe/controllers/WireController.java
@@ -1,13 +1,17 @@
 package org.cafebabe.controllers;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import org.cafebabe.model.components.connections.Wire;
 import org.cafebabe.model.workspace.Position;
 
 public class WireController {
 
+    private static Color activeColor = Color.color(234, 38, 38, 1);
+    private static Color inactiveColor = Color.color(0, 0, 0, 1);
+
     private Wire wire;
-    private Position startPoint;
-    private Position endPoint;
+    private Line wireLine;
 
     WireController(Wire wire, int startX, int startY, int endX, int endY) {
         this(wire, new Position(startX, startY), new Position(endX, endY));
@@ -15,23 +19,40 @@ public class WireController {
 
     WireController(Wire wire, Position startPoint, Position endPoint) {
         this.wire = wire;
-        this.startPoint = startPoint;
-        this.endPoint = endPoint;
+        this.wireLine = new Line(startPoint.getX(), startPoint.getY(), endPoint.getX(), endPoint.getY());
+        this.wireLine.setFill(getWireColor());
+        this.wire.onStateChangedEvent().addListener(this::updateState);
     }
 
     void moveStartPointTo(int x, int y) {
-        this.startPoint.move(x, y);
+        this.wireLine.setStartX(x);
+        this.wireLine.setStartY(y);
     }
 
     void moveEndPointTo(int x, int y) {
-        this.endPoint.move(x, y);
+        this.wireLine.setEndX(x);
+        this.wireLine.setEndY(y);
     }
 
     void translateStartPoint(int deltaX, int deltaY) {
-        this.startPoint.translate(deltaX, deltaY);
+        this.wireLine.setStartX(wireLine.getStartX() + deltaX);
+        this.wireLine.setStartY(wireLine.getStartY() + deltaY);
     }
 
     void translateEndPoint(int deltaX, int deltaY) {
-        this.endPoint.translate(deltaX, deltaY);
+        this.wireLine.setEndX(wireLine.getEndX() + deltaX);
+        this.wireLine.setEndY(wireLine.getEndY() + deltaY);
+    }
+
+    private Color getWireColor() {
+        return (this.wire.isActive()) ? activeColor : inactiveColor;
+    }
+
+    public Line getWireLine() {
+        return this.wireLine;
+    }
+
+    void updateState(Wire wire) {
+        this.wireLine.setStroke(getWireColor());
     }
 }
