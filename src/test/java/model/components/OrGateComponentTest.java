dbfc827e52a3d9942c2b927dbc920628e4b4603a
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-09-20 09:35:28 +0200

    Add unit test for new components

---
+++ src/test/java/model/components/OrGateComponentTest.java
@@ -0,0 +1,44 @@
+package model.components;
+
+import org.cafebabe.model.components.OrGateComponent;
+import org.cafebabe.model.components.PowerSourceComponent;
+import org.cafebabe.model.components.connections.Wire;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrGateComponentTest {
+
+    @Test
+    void metadataTest() {
+        OrGateComponent comp = new OrGateComponent();
+        assertEquals("OR_ANSI", comp.getAnsiName());
+        assertEquals("OR-Gate", comp.getDisplayName());
+        assertEquals("Emits an active signal if either inputs are active", comp.getDescription());
+    }
+
+    @Test
+    void intendedUseTest() {
+        PowerSourceComponent power1 = new PowerSourceComponent();
+        PowerSourceComponent power2 = new PowerSourceComponent();
+        Wire in1 = new Wire();
+        Wire in2 = new Wire();
+        Wire out = new Wire();
+        OrGateComponent comp = new OrGateComponent();
+
+        // Connect wires to power sources
+        power1.connectToPort(in1, "north");
+        power2.connectToPort(in2, "north");
+
+        // initial state should off
+        comp.connectToPort(out, "output");
+        assertFalse(out.isActive());
+
+        // OR-Gate output should be 1 when either inputs are on
+        comp.connectToPort(in1, "input1");
+        assertTrue(out.isActive());
+        comp.connectToPort(in2, "input2");
+        assertTrue(out.isActive());
+
+    }
+}
