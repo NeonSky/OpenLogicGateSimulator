7ab510bdd24c82d0254d0e12b1c046bce5c0f6b5
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-09-20 09:35:28 +0200

    Add unit test for new components

---
+++ src/test/java/model/components/NorGateComponentTest.java
@@ -0,0 +1,47 @@
+package model.components;
+
+import org.cafebabe.model.components.NorGateComponent;
+import org.cafebabe.model.components.PowerSourceComponent;
+import org.cafebabe.model.components.connections.Wire;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NorGateComponentTest {
+
+    @Test
+    void metadataTest() {
+        NorGateComponent comp = new NorGateComponent();
+        assertEquals("NOR_ANSI", comp.getAnsiName());
+        assertEquals("NOR-Gate", comp.getDisplayName());
+        assertEquals("Emits no signal if any inputs are active", comp.getDescription());
+    }
+
+    @Test
+    void intendedUseTest() {
+        PowerSourceComponent power1 = new PowerSourceComponent();
+        PowerSourceComponent power2 = new PowerSourceComponent();
+        Wire in1 = new Wire();
+        Wire in2 = new Wire();
+        Wire out = new Wire();
+        NorGateComponent comp = new NorGateComponent();
+
+        // Connect wires to power sources
+        power1.connectToPort(in1, "north");
+        power2.connectToPort(in2, "north");
+
+        comp.connectToPort(out, "output");
+
+        // NOR-Gate output should be 0 when either inputs are on
+        comp.connectToPort(in1, "input1");
+        assertFalse(out.isActive());
+        comp.connectToPort(in2, "input2");
+        assertFalse(out.isActive());
+
+    }
+
+    @Test
+    void noSelfEnergyTest() {
+        // TODO
+    }
+}
---
+++ src/test/java/model/components/XorGateComponentTest.java
@@ -0,0 +1,44 @@
+package model.components;
+
+import org.cafebabe.model.components.XorGateComponent;
+import org.cafebabe.model.components.PowerSourceComponent;
+import org.cafebabe.model.components.connections.Wire;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XorGateComponentTest {
+
+    @Test
+    void metadataTest() {
+        XorGateComponent comp = new XorGateComponent();
+        assertEquals("XOR_ANSI", comp.getAnsiName());
+        assertEquals("XOR-Gate", comp.getDisplayName());
+        assertEquals("Emits an active signal if exactly one input is active", comp.getDescription());
+    }
+
+    @Test
+    void intendedUseTest() {
+        PowerSourceComponent power1 = new PowerSourceComponent();
+        PowerSourceComponent power2 = new PowerSourceComponent();
+        Wire in1 = new Wire();
+        Wire in2 = new Wire();
+        Wire out = new Wire();
+        XorGateComponent comp = new XorGateComponent();
+
+        // Connect wires to power sources
+        power1.connectToPort(in1, "north");
+        power2.connectToPort(in2, "north");
+
+        // initial state should off
+        comp.connectToPort(out, "output");
+        assertFalse(out.isActive());
+
+        // XOR-Gate output should only be 1 when only one input is active
+        comp.connectToPort(in1, "input1");
+        assertTrue(out.isActive());
+        comp.connectToPort(in2, "input2");
+        assertFalse(out.isActive());
+
+    }
+}
