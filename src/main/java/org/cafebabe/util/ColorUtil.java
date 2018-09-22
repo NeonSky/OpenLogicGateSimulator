ed29c830442488f2585dacb8a3a20188a89dfffb
Jimmy M Andersson <hello@applyn.se>
2018-09-22 13:16:46 +0200

    Refactor selection colors into new ColorUtil class

---
+++ src/main/java/org/cafebabe/util/ColorUtil.java
@@ -0,0 +1,9 @@
+package org.cafebabe.util;
+
+import javafx.scene.paint.Color;
+
+public class ColorUtil {
+    public static final Color HIGH_COLOR = Color.color(234.0/255, 38.0/255, 38.0/255, 1);
+    public static final Color LOW_COLOR = Color.color(0, 0, 0, 1);
+    public static final Color SELECTED_COLOR = Color.color(110.0/255, 134.0/255, 148.0/255, 1);
+}
