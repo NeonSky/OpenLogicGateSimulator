70ae36d369601db8242f841e3e2941b342ac84da
Jimmy M Andersson <hello@applyn.se>
2018-09-24 11:37:18 +0200

    Rebase and rebase fixes

--- src/main/java/org/cafebabe/util/ColorUtil.java
+++ src/main/java/org/cafebabe/util/ColorUtil.java
@@ -3,8 +3,8 @@ package org.cafebabe.util;
 import javafx.scene.paint.Color;
 
 public class ColorUtil {
-    public static final Color HIGH = Color.color(234.0/255, 38.0/255, 38.0/255, 1);
-    public static final Color LOW = Color.color(0, 0, 0, 1);
+    public static final Color ACTIVE = Color.color(234.0/255, 38.0/255, 38.0/255, 1);
+    public static final Color INACTIVE = Color.color(0, 0, 0, 1);
     public static final Color SELECTED = Color.color(110.0/255, 134.0/255, 148.0/255, 1);
     public static final Color UNDEFINED = Color.color(204.0/255, 204.0/255, 0, 1);
 }
