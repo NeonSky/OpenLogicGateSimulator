abaa08299ff59a95bf8a444ca2f98e0e9b530687
Elias Sundqvist <elias@sundqvist.com>
2018-09-27 20:21:45 +0200

    Fix invisible wires

--- src/main/java/org/cafebabe/model/util/ComponentUtil.java
+++ src/main/java/org/cafebabe/model/util/ComponentUtil.java
@@ -1,42 +1,16 @@
 package org.cafebabe.model.util;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import org.cafebabe.controllers.util.SvgUtil;
 import org.cafebabe.model.components.Component;
 import org.cafebabe.model.components.ComponentConstructor;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
-
 public class ComponentUtil {
-    public static List<Component> getAllComponents() {
-        Set<Constructor> constructors = new Reflections("org.cafebabe.model.components", new MethodAnnotationsScanner())
-                .getConstructorsAnnotatedWith(ComponentConstructor.class);
-
-        List<Component> components = new ArrayList<>();
-        for(Constructor constructor: constructors) {
-            try {
-                if(Component.class.isAssignableFrom(constructor.getDeclaringClass())) {
-                    components.add((Component) constructor.newInstance());
-                }
-            } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-        return components;
-    }
-
-    private static Map<String, Component> getComponentMap() {
-        Map<String, Component> map = new HashMap<>();
-
-        for (Component c : getAllComponents()) {
-            map.put(c.getDisplayName(), c);
-        }
-
-        return map;
-    }
-
+    /* Public */
     public static Component componentFactory(String displayName) {
         if (displayName == null || displayName.isEmpty()) {
             throw new RuntimeException("Component display name can not be null or empty");
@@ -52,11 +26,41 @@ public class ComponentUtil {
 
         try {
             Constructor ctor = original.getClass().getConstructor();
-            return (Component)ctor.newInstance();
+            Component instance = (Component) ctor.newInstance();
+            instance.initPorts(SvgUtil.getComponentMetadata(instance));
+            return instance;
         } catch (IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    public static List<Component> getAllComponents() {
+        Set<Constructor> constructors = new Reflections("org.cafebabe.model.components", new MethodAnnotationsScanner())
+                .getConstructorsAnnotatedWith(ComponentConstructor.class);
+
+        List<Component> components = new ArrayList<>();
+        for (Constructor constructor : constructors) {
+            try {
+                if (Component.class.isAssignableFrom(constructor.getDeclaringClass())) {
+                    components.add((Component) constructor.newInstance());
+                }
+            } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        return components;
+    }
+
+    /* Private */
+    private static Map<String, Component> getComponentMap() {
+        Map<String, Component> map = new HashMap<>();
+
+        for (Component c : getAllComponents()) {
+            map.put(c.getDisplayName(), c);
+        }
+
+        return map;
     }
 }
