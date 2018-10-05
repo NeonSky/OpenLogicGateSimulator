1cc6e87e70b9d25d5bbb591d7a2c46e41eb0533e
Jimmy M Andersson <hello@applyn.se>
2018-10-05 18:17:21 +0200

    Add initial workspace and ability to close down workspaces

--- src/main/java/org/cafebabe/controllers/editor/EditorViewController.java
+++ src/main/java/org/cafebabe/controllers/editor/EditorViewController.java
@@ -1,9 +1,16 @@
 package org.cafebabe.controllers.editor;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
@@ -18,12 +25,31 @@ public class EditorViewController implements Initializable {
     @FXML private AnchorPane sidebarAnchorPane;
     @FXML private AnchorPane workspacesPane;
     @FXML private TabPane tabsPane;
+    private final List<WorkspaceController> workspaces = new ArrayList<>();
 
     /* Public */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initializeSidebar();
         initializeWorkspace();
+        initializeTabPane();
+    }
+
+    public void addNewWorkspace() {
+        Workspace workspace = new Workspace();
+        WorkspaceController workspaceController = new WorkspaceController(workspace);
+        this.workspaces.add(workspaceController);
+        addWorkspaceTab(workspaceController);
+    }
+
+    public void removeWorkspace(Event event, WorkspaceController workspaceController) {
+        event.consume();
+        this.workspacesPane.getChildren().remove(workspaceController);
+        this.workspaces.remove(workspaceController);
+
+        if (this.workspaces.isEmpty()) {
+            addNewWorkspace();
+        }
     }
 
     /* Private */
@@ -32,8 +58,33 @@ public class EditorViewController implements Initializable {
     }
 
     private void initializeWorkspace() {
-        this.workspacesPane.getChildren().add(new WorkspaceController(new Workspace()));
-
+        addNewWorkspace();
+        this.workspacesPane.getChildren().addAll(this.workspaces.get(0));
         this.workspacesPane.requestLayout();
+    }
+
+    private void initializeTabPane() {
+        SingleSelectionModel<Tab> model = this.tabsPane.getSelectionModel();
+        ReadOnlyIntegerProperty selected = model.selectedIndexProperty();
+        selected.addListener((observable, oldValue, newValue) -> selectWorkspace(newValue));
+    }
+
+    private void selectWorkspace(Number index) {
+        if (index.intValue() < 0) {
+            return;
+        }
+
+        this.workspacesPane.getChildren().clear();
+        WorkspaceController selectedWorkspace = this.workspaces.get(index.intValue());
+        this.workspacesPane.getChildren().add(selectedWorkspace);
+    }
+
+    private void addWorkspaceTab(WorkspaceController workspaceController) {
+        Tab tab = new Tab();
+        tab.setText("New Workspace");
+        tab.setClosable(true);
+        tab.setOnClosed(event -> this.removeWorkspace(event, workspaceController));
+        this.tabsPane.getTabs().add(tab);
+        this.tabsPane.getSelectionModel().select(tab);
     }
 }
