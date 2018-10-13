32506887cb74614413b5c2ead61c21e9debb7312
Elias Sundqvist <elias@sundqvist.com>
2018-10-14 01:25:56 +0200

    Add toggle method to SignalSource

--- src/main/java/org/cafebabe/model/editor/workspace/circuit/component/source/SignalSourceComponent.java
+++ src/main/java/org/cafebabe/model/editor/workspace/circuit/component/source/SignalSourceComponent.java
@@ -3,6 +3,7 @@ package org.cafebabe.model.editor.workspace.circuit.component.source;
 import java.util.Map;
 import org.cafebabe.model.editor.workspace.circuit.component.Component;
 import org.cafebabe.model.editor.workspace.circuit.component.ComponentConstructor;
+import org.cafebabe.model.editor.workspace.circuit.component.connection.LogicState;
 import org.cafebabe.model.editor.workspace.circuit.component.connection.OutputPort;
 
 /**
@@ -10,12 +11,16 @@ import org.cafebabe.model.editor.workspace.circuit.component.connection.OutputPo
  */
 public class SignalSourceComponent extends Component {
 
+    private final OutputPort northOutput;
+
     @ComponentConstructor
     public SignalSourceComponent() {
-        OutputPort northOutput = new OutputPort();
-        tagToOutput = Map.ofEntries(
-                Map.entry("output", northOutput));
-        setOutputState(northOutput, true);
+        this.northOutput = new OutputPort();
+        this.tagToOutput = Map.ofEntries(
+                Map.entry("output", this.northOutput)
+        );
+
+        setOutputState(this.northOutput, true);
     }
 
     /* Public */
@@ -36,5 +41,14 @@ public class SignalSourceComponent extends Component {
 
     @Override
     protected void updateOutputs() {}
+
+    public void toggle() {
+        this.toggle(!this.northOutput.isHigh());
+    }
+
+    public void toggle(Boolean setHigh) {
+        this.northOutput.setState(setHigh ? LogicState.HIGH : LogicState.LOW);
+    }
+
 
 }
