d257cb7bf1c5a00371a401f6b53cbddd94f51871
Elias Sundqvist <elias@sundqvist.com>
2018-10-18 01:44:29 +0200

    Fix review suggestions

--- src/main/java/org/cafebabe/model/editor/workspace/circuit/component/feedback/HexDisplayComponent.java
+++ src/main/java/org/cafebabe/model/editor/workspace/circuit/component/feedback/HexDisplayComponent.java
@@ -12,32 +12,22 @@ import org.cafebabe.model.editor.workspace.circuit.component.connection.InputPor
  */
 public class HexDisplayComponent extends Component {
 
-    protected final List<InputPort> inputs;
-    private static final int[] INT_TO_SEG = new int[]{
+    private static final int[] INT_TO_SEG = new int[] {
             0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
             0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71
     };
+    private final List<InputPort> inputs = new ArrayList<>();
 
     @ComponentConstructor
     public HexDisplayComponent() {
         super("HEX_Display", "Hex Display", "Displays a hexadecimal representaion of its input.");
-        this.inputs = new ArrayList<>();
         tagToInput = new HashMap<>();
         for (int i = 0; i < 7; i++) {
             addInputAtIndex(i);
         }
     }
 
-    private void addInputAtIndex(int i) {
-        InputPort port = new InputPort();
-        tagToInput.put("input" + i, port);
-        this.inputs.add(i, port);
-        port.onStateChangedEvent().addListener(p -> updateOutputs());
-        removeStateData("show-segment-" + (6 - i));
-    }
-
-    /* Public */
-
+    /* Protected */
     @Override
     protected void updateOutputs() {
         int number = 0;
@@ -53,5 +43,14 @@ public class HexDisplayComponent extends Component {
                 removeStateData("show-segment-" + (6 - i));
             }
         }
+    }
+
+    /* Private */
+    private void addInputAtIndex(int i) {
+        InputPort port = new InputPort();
+        tagToInput.put("input" + i, port);
+        this.inputs.add(i, port);
+        port.onStateChangedEvent().addListener(p -> updateOutputs());
+        removeStateData("show-segment-" + (6 - i));
     }
 }
