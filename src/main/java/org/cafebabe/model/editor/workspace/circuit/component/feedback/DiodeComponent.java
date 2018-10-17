674c7ad8b6bf1d9304d0a492ab30daec347b4bb2
Elias Sundqvist <elias@sundqvist.com>
2018-10-17 18:33:28 +0200

    Add basic diode component

---
+++ src/main/java/org/cafebabe/model/editor/workspace/circuit/component/feedback/DiodeComponent.java
@@ -0,0 +1,29 @@
+package org.cafebabe.model.editor.workspace.circuit.component.feedback;
+
+import java.util.Map;
+import org.cafebabe.model.editor.workspace.circuit.component.Component;
+import org.cafebabe.model.editor.workspace.circuit.component.ComponentConstructor;
+import org.cafebabe.model.editor.workspace.circuit.component.connection.InputPort;
+
+/**
+ * A component that lights up on a high signal.
+ */
+public class DiodeComponent extends Component {
+
+    private final InputPort input;
+
+    @ComponentConstructor
+    public DiodeComponent() {
+        super("DIODE_Component", "DIODE", "Is lit up bright yellow on high signal.");
+        this.input = new InputPort();
+        tagToInput = Map.ofEntries(
+                Map.entry("input", this.input)
+        );
+
+        this.input.onStateChangedEvent().addListener(p -> updateOutputs());
+    }
+
+    public boolean isLit() {
+        return this.input.isHigh();
+    }
+}
