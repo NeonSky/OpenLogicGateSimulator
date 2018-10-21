1edb1b4d3cc5a8e6d8d5f6b8f6e43b11fff3aea6
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2018-10-21 15:42:20 +0200

    Full XNOR coverage

--- src/test/java/model/components/XnorGateComponentTest.java
+++ src/test/java/model/components/XnorGateComponentTest.java
@@ -1,6 +1,5 @@
 package model.components;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.cafebabe.model.editor.workspace.circuit.component.connection.Wire;
@@ -13,7 +12,7 @@ class XnorGateComponentTest {
 
     /* Package-Private */
     @Test
-    void intendedUseTest() {
+    void bothInputsHighShouldGiveHighOutput() {
         SignalSourceComponent power1 = new SignalSourceComponent();
         SignalSourceComponent power2 = new SignalSourceComponent();
         Wire in1 = new Wire();
@@ -30,7 +29,29 @@ class XnorGateComponentTest {
         // XNOR-Gate output should only be 1 when amount of active
         // inputs is not equal to 1
         comp.connectToPort(in1, "input1");
-        assertFalse(out.isHigh());
+        comp.connectToPort(in2, "input2");
+        assertTrue(out.isHigh());
+    }
+
+    @Test
+    void bothInputsLowShouldGiveHighOutput() {
+        SignalSourceComponent power = new SignalSourceComponent();
+        power.toggle(); // turn the power off
+
+        Wire in1 = new Wire();
+        Wire in2 = new Wire();
+        Wire out = new Wire();
+        XnorGateComponent comp = new XnorGateComponent();
+
+        // Connect wires to power source
+        power.connectToPort(in1, "output");
+        power.connectToPort(in2, "output");
+
+        comp.connectToPort(out, "output");
+
+        // XNOR-Gate output should only be 1 when amount of active
+        // inputs is not equal to 1
+        comp.connectToPort(in1, "input1");
         comp.connectToPort(in2, "input2");
         assertTrue(out.isHigh());
     }
